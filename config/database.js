2dc57af0c7d0b5aaba5db5b229f715f8e0225b1a
Hersin Kevin <khersinpro@gmail.com>
2023-10-26 13:41:15 +0200

    add env for cross environment platform

--- config/database.js
+++ config/database.js
@@ -1,11 +1,10 @@
-require('dotenv').config()
-const env = process.env
-
+const env = require('dotenv').config({path: __dirname + '/../.env'}).parsed;
+console.log(env);
 module.exports = {
     username: env.USER,
     password: env.PASSWORD,
     database: env.DATABASE,
     host: env.HOST,
-    port: env.PORT,
+    port: env.DB_PORT,
     dialect: env.DIALECT
 }
